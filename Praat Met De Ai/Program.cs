9990a7d15e9bfe04e17de43b765b3bdd664ee951
danuiii <danu@danujanssen.nl>
2021-03-03 16:58:21 +0100

    Add project files.

---
+++ Praat Met De Ai/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace Praat_Met_De_Ai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kleur van de text aan passen.
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            // Praat met de AI.
+
+            Console.WriteLine("Bliep Bloep Blap");
+
+            Console.WriteLine("Hallo, Ik ben een AI. En jou naam is?");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Cool! Wat is je favoriete kleur?");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Mijn favoriete kleur is ConsoleColor.Green;");
+
+            Console.WriteLine("Ik zou je is een verhaaltje vertellen over een ridder, EN JIJ MAG MEE HELPEN :)");
+
+            Console.WriteLine("Een coole ridder genaamd . . .");
+
+            Console.ReadLine();
+
+            Console.WriteLine("En zijn hulpje . . .");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Werden op een dag opgegeten door een draak genaamd . . .");
+
+            Console.ReadLine();
+
+            Console.Write("EINDE!!! Wat vond je ervan?");
+
+            Console.ReadLine();
+
+            Console.WriteLine("Dankje, dankje. Nou ik moet maar eens gaan. Doei doei!!!");
+
+            Console.WriteLine("Klik 2 keer op spatie om weg te gaan.");
+
+            Console.ReadKey();
+
+        }
+    }
+}
